7ad79249a97eb2d2476fb2e0af3c9a117aac460a
Alejandro Medina <5almero5@gmail.com>
2021-02-16 16:19:26 -0600

    Add some grid examples

--- src/Components/AppLayout/App.tsx
+++ src/Components/AppLayout/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { Typography, Link } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { theme } from '../../styles/theme';
-import{
-  CustomCheckbox
-} from '../CustomCheckbox';
 
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
 function App() {
-  const preventDefault = (event: React.SyntheticEvent) => event.preventDefault();
+  const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
-    <div className="App">
-        <Typography color='secondary' variant='h1'>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </Typography>
-      <Typography>
-        <Link  href="https://reactjs.org" onClick={preventDefault}>
-        Learn React
-        </Link>
-      </Typography>
+       <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper} >xs=12</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper} >xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper} >xs=6</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} >xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} >xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} >xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper} >xs=3</Paper>
+        </Grid>
+      </Grid>
     </div>
-      <CustomCheckbox />
     </ThemeProvider>
   );
 }
