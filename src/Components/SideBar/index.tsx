16d65b97b412e12b61bf4c2ae8ad1402985f3995
Alejandro Medina <5almero5@gmail.com>
2021-03-12 16:45:30 -0600

    less items

--- src/Components/SideBar/index.tsx
+++ src/Components/SideBar/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {
   makeStyles,
   useTheme,
@@ -6,6 +6,7 @@ import {
   createStyles,
 } from "@material-ui/core/styles";
 import {Drawer, List, Divider, IconButton, ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
+
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
@@ -112,16 +113,6 @@ export function SideBar({openBar, closer}: ISideBar) {
           ))}
         </List>
         <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
       </Drawer>
 
   );
