9c30462609a5ff38c68cad661b508f989e1d724d
Alejandro Medina <5almero5@gmail.com>
2021-04-06 21:21:58 -0500

    Add types & interface

---
+++ src/Interfaces/index.ts
@@ -0,0 +1,44 @@
+
+export interface NamedAPIResource  {
+    url	: string;
+    name: string;
+}
+export interface Language {
+    id: number;
+    name: string;
+    official: boolean;
+    iso3166: string;
+    iso639: string;
+    names: Name[];
+}
+export interface Name {
+    name: string;
+    language: Language;
+}
+export interface GenerationGameIndex  {
+    game_index: number;
+    generation: NamedAPIResource
+}
+export interface TypePokemon {
+    slot: number;
+    pokemon: NamedAPIResource;
+}
+export interface POKETYPE_API {
+    id: number;
+    name: string;
+    damage_relations: TYPE_RELATION;
+    game_indices: GenerationGameIndex;
+    generation:  NamedAPIResource;
+    move_damage_class: NamedAPIResource;
+    names: Name[];
+    pokemon: TypePokemon;
+    moves: NamedAPIResource;
+}
+export interface TYPE_RELATION {
+    no_damage_to: NamedAPIResource[];
+    half_damage_to: NamedAPIResource[];
+    double_damage_to: NamedAPIResource[];
+    no_damage_from: NamedAPIResource[];
+    half_damage_from: NamedAPIResource[];
+    double_damage_from: NamedAPIResource[];
+}
--- src/Utils/api/types.ts
+++ src/Utils/api/types.ts
@@ -1,57 +1,16 @@
-import {API_POKE_TYPES} from '../consts';
-interface NamedAPIResource  {
-    url	: string;
-    name: string;
-}
-interface Language {
-    id: number;
-    name: string;
-    official: boolean;
-    iso3166: string;
-    iso639: string;
-    names: Name[];
-}
-interface Name {
-    name: string;
-    language: Language;
-}
-interface GenerationGameIndex  {
-    game_index: number;
-    generation: NamedAPIResource
-}
-interface TypePokemon {
-    slot: number;
-    pokemon: NamedAPIResource;
-}
-interface POKETYPE_API {
-    id: number;
-    name: string;
-    damage_relations: TYPE_RELATION;
-    game_indices: GenerationGameIndex;
-    generation:  NamedAPIResource;
-    move_damage_class: NamedAPIResource;
-    names: Name[];
-    pokemon: TypePokemon;
-    moves: NamedAPIResource;
-}
-interface TYPE_RELATION {
-    no_damage_to: NamedAPIResource[];
-    half_damage_to: NamedAPIResource[];
-    double_damage_to: NamedAPIResource[];
-    no_damage_from: NamedAPIResource[];
-    half_damage_from: NamedAPIResource[];
-    double_damage_from: NamedAPIResource[];
-}
-export async function fetchTypes(){
+import { API_POKE_TYPES } from '../consts';
+import { NamedAPIResource, POKETYPE_API } from '../../Interfaces'
 
+export async function fetchTypes(): Promise<POKETYPE_API[]>{
+    let pokeTypes: Array<POKETYPE_API>;
     const response = await fetch(API_POKE_TYPES)
     const {results} =  await response.json()
 
-    const  pokeTypes = await Promise.all(results.map(async (poketype: NamedAPIResource) => {
+    pokeTypes = await Promise.all(results.map(async (poketype: NamedAPIResource) => {
         const contents = await fetch(poketype.url)
         const res =  await contents.json()
         return res
     }));
-    console.log('pokeTypes', pokeTypes)
+
     return pokeTypes;
 }
