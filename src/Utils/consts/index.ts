d28bcf9a6273beda64f4f99b89183b51f4a2af13
Alejandro Medina <5almero5@gmail.com>
2021-04-13 14:25:33 -0500

    add utils

--- src/Utils/consts/index.ts
+++ src/Utils/consts/index.ts
@@ -3,3 +3,5 @@ const URL = 'pokeapi.co/api';
 const VERSION ='v2';
 export const POKE_URL = `${PROTOCOL}${URL}/${VERSION}`;
 export const API_POKE_TYPES = `${POKE_URL}/type`;
+
+export const URL_FRONT_SPRITES = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
---
+++ src/Utils/pokemons/index.ts
@@ -0,0 +1,3 @@
+import {URL_FRONT_SPRITES} from '../consts';
+
+export const  getPokemonSprite = (id: number) => `${URL_FRONT_SPRITES}${id}.png`;
