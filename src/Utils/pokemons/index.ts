78edf4df1264d21c111c693c7253ede71a18bcfd
Alejandro Medina <5almero5@gmail.com>
2021-04-14 09:21:32 -0500

    add regext tool

--- src/Utils/pokemons/index.ts
+++ src/Utils/pokemons/index.ts
@@ -1,3 +1,9 @@
 import {URL_FRONT_SPRITES} from '../consts';
 
 export const  getPokemonSprite = (id: number) => `${URL_FRONT_SPRITES}${id}.png`;
+
+export const getIdByUrl = (url: string) => {
+    let pathId = /\/(\d+)\//g;
+    const match = url.match(pathId);
+    console.log(match);
+}
