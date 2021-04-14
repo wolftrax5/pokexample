47e4d6a87a0e5ace990ef5b457ee74fe59748949
Alejandro Medina <5almero5@gmail.com>
2021-04-14 09:21:41 -0500

    add empty values

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -3,6 +3,10 @@ export interface NamedAPIResource  {
     url	: string;
     name: string;
 }
+export const EMPTY_NamedAPIResource = {
+    url	: '',
+    name: '',
+}
 export interface Language {
     id: number;
     name: string;
@@ -19,9 +23,33 @@ export interface GenerationGameIndex  {
     game_index: number;
     generation: NamedAPIResource
 }
+export const EMPTY_GenerationGameIndex:GenerationGameIndex= {
+    game_index: 0,
+    generation: EMPTY_NamedAPIResource
+}
 export interface TypePokemon {
     slot: number;
     pokemon: NamedAPIResource;
+}
+export const EMPTY_TypePokemon:TypePokemon= {
+    slot: 0,
+    pokemon: EMPTY_NamedAPIResource
+}
+export interface TYPE_RELATION {
+    no_damage_to: NamedAPIResource[];
+    half_damage_to: NamedAPIResource[];
+    double_damage_to: NamedAPIResource[];
+    no_damage_from: NamedAPIResource[];
+    half_damage_from: NamedAPIResource[];
+    double_damage_from: NamedAPIResource[];
+}
+export const  EMPTY_TYPE_RELATION:TYPE_RELATION = {
+    no_damage_to: [],
+    half_damage_to:[],
+    double_damage_to: [],
+    no_damage_from: [],
+    half_damage_from: [],
+    double_damage_from: [],
 }
 export interface POKETYPE_API {
     id: number;
@@ -31,16 +59,19 @@ export interface POKETYPE_API {
     generation:  NamedAPIResource;
     move_damage_class: NamedAPIResource;
     names: Name[];
-    pokemon: TypePokemon;
+    pokemon: TypePokemon[];
     moves: NamedAPIResource;
 }
-export interface TYPE_RELATION {
-    no_damage_to: NamedAPIResource[];
-    half_damage_to: NamedAPIResource[];
-    double_damage_to: NamedAPIResource[];
-    no_damage_from: NamedAPIResource[];
-    half_damage_from: NamedAPIResource[];
-    double_damage_from: NamedAPIResource[];
+export const EMPTY_POKETYPE_API:POKETYPE_API = {
+    id: 0,
+    name: '',
+    damage_relations:EMPTY_TYPE_RELATION,
+    game_indices: EMPTY_GenerationGameIndex,
+    generation: EMPTY_NamedAPIResource,
+    move_damage_class: EMPTY_NamedAPIResource,
+    names: [],
+    pokemon: [],
+    moves: EMPTY_NamedAPIResource,
 }
 
 export interface REDUCER_ACTION {
